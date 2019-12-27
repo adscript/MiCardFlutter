ef68289aa795058e62dc9308b513de1a0e2282ce
adscript <adnanocode@gmail.com>
2019-12-27 16:26:44 +0700

    First Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/adnan.png'),
+              ),
+              Text(
+                'Adnan Radja Maulana',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'MOBILE DEVELOPER ',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250,
+                child: Divider(
+                  color: Colors.teal.shade100,
+
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+62 852 9428 9367',
+                      style: TextStyle(
+                        fontFamily: 'SouceSansPro',
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'adnanocode@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'SouceSansPro',
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                )
+              )
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
